e4abdfd0855499a21776576fd59855d026e4ff10
sabita <sabitakhadka091@gmail.com>
2024-03-18 11:46:08 +0545

    learned about tuples in ts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-"use strict";
 // Tuples types in ts
-const data = ["Hey", "Hello", 1, 2];
+var data = ["Hey", "Hello", 1, 2];
 data.push(true);
 console.log(data);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-// Configuration file in ts
+// Tuples types in ts
+const data: [string, string, number, number, boolean?] = ["Hey", "Hello", 1, 2];
+data.push(true);
+console.log(data);
